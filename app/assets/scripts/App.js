b3dc450b927520bce3a88e26d3af8b7e6130b70f
Rafael Matera <rmaterapay@gmail.com>
2025-07-19 17:45:00 -0600

    Completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import "../styles/styles.css"
 
 import MobileMenu from "./modules/MobileMenu"
+import ReavealOnScroll from "./modules/RevealOnScroll"
 
 new MobileMenu()
+new ReavealOnScroll(document.querySelectorAll(".service-item"), 75)
+new ReavealOnScroll(document.querySelectorAll(".pricing-item"), 75)
 
 if (module.hot) {
   module.hot.accept()
