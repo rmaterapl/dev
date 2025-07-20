eedf1701c6f92c150f66819d2f6698bd615cf6ca
Rafael Matera <rmaterapay@gmail.com>
2025-07-20 00:05:35 -0600

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,30 @@
 import "../styles/styles.css"
-
 import MobileMenu from "./modules/MobileMenu"
 import ReavealOnScroll from "./modules/RevealOnScroll"
 import Header from "./modules/Header"
+import { forEach } from "lodash"
 
 new Header()
 new MobileMenu()
 new ReavealOnScroll(document.querySelectorAll(".service-item"), 75)
 new ReavealOnScroll(document.querySelectorAll(".pricing-item"), 75)
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import("./modules/Modal")
+        .then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openModal(), 20)
+        })
+        .catch(() => console.log("There was a problem. Please try again."))
+    } else {
+      modal.openModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
