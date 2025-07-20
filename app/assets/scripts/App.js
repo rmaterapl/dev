bc560e20eed1fedc3e67f02691bf1a29aaace265
Rafael Matera <rmaterapay@gmail.com>
2025-07-19 18:39:44 -0600

    Headear js completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import "../styles/styles.css"
 
 import MobileMenu from "./modules/MobileMenu"
 import ReavealOnScroll from "./modules/RevealOnScroll"
+import Header from "./modules/Header"
 
+new Header()
 new MobileMenu()
 new ReavealOnScroll(document.querySelectorAll(".service-item"), 75)
 new ReavealOnScroll(document.querySelectorAll(".pricing-item"), 75)
